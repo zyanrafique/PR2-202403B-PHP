9f12cc322f8e2903951a9628ecdd16daa9535bcd
zyan <zyanaptech@gmail.com>
2025-02-28 11:01:26 +0500

    tempate slicing jo downlaod kiya ha esma admin panel bnay ha

---
+++ adminpanel/addCategory.php
@@ -0,0 +1,20 @@
+<?php
+include('component/header.php')
+?>
+
+   <!-- Blank Start -->
+   <div class="container-fluid pt-4 px-4">
+                <div class="row vh-100 bg-light rounded align-items-center justify-content-center mx-0">
+                    <div class="col-md-6 text-center">
+                        
+                    </div>
+                </div>
+            </div>
+            <!-- Blank End -->
+
+
+
+
+<?php
+include('component/footer.php')
+?>
